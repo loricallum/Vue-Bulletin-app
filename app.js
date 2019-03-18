c640ce480bdd651a1818e395dd2ef2abd8c7661d
LLewis <lorimlewis@gmail.com>
2019-03-18 22:28:35 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+
+//target the div with an id of EVENTS
+el:'#events',
+
+//can register any values or collections that hold data for the app
+data: {
+},
+
+//Anything here will run when application loads
+ready: function(){
+
+},
+//Methods to use in app are registered here
+methods: {
+
+}
+});
